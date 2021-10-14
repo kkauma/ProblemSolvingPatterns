aa5e1840b076e6146278c564baa530ca46b285bc
Kyle Kauma <kylekauma@gmail.com>
2021-10-13 21:03:50 -0500

    add multiple pointers instructions for sumZero problem

---
+++ multiplePointers.js
@@ -0,0 +1,4 @@
+// Write a function called sumZero which accepts a sorted array of
+// integers. The function should find the first pair whre the sum
+// is 0. Return an array that includes both values that sum to zero
+// or undefined if a pair does not exist
