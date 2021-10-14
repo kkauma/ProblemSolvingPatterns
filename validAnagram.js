5a07e8f1fba59a95f9565905624f0cd7c5b0471e
Kyle Kauma <kylekauma@gmail.com>
2021-10-13 20:37:22 -0500

    finish implenting logic for initial anagram solution

--- validAnagram.js
+++ validAnagram.js
@@ -12,7 +12,22 @@ const validAnagram = (str1, str2) => {
   for (let letter of str1) {
     frequencyCounter1[letter] = (frequencyCounter1[letter] || 0) + 1;
   }
-  console.log(frequencyCounter1);
+  for (let letter of str2) {
+    frequencyCounter2[letter] = (frequencyCounter2[letter] || 0) + 1;
+  }
+  //   console.log(frequencyCounter1);
+  //   console.log(frequencyCounter2);
+
+  for (let key in frequencyCounter1) {
+    if (!(key in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-validAnagram("hello", "ohlle"); // should be true
+console.log(validAnagram("hello", "ohlle")); // true
+console.log(validAnagram("hello", "world")); // false
