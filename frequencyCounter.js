617079b43fda2e62a3cbb228c68868c19f8109b9
Kyle Kauma <kylekauma@gmail.com>
2021-10-13 19:26:02 -0500

    add optimal solution, runtime is linear

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -8,17 +8,47 @@
 // The frequency of values must be the same
 
 // Naive solution, O(n^2)
+// function same(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2);
+//     if (correctIndex === -1) {
+//       return false;
+//     }
+//     console.log(arr2);
+//     arr2.splice(correctIndex, 1);
+//   }
+//   return true;
+// }
+
+// Refactored, O(3n) or just O(n), linear time
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2);
-    if (correctIndex === -1) {
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
       return false;
     }
-    console.log(arr2);
-    arr2.splice(correctIndex, 1);
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
   }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
   return true;
 }
+
+console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
+console.log(same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11]));
