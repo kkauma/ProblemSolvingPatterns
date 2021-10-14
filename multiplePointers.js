c185c73cb6b099c87a42041de240ac04557b9559
Kyle Kauma <kylekauma@gmail.com>
2021-10-13 21:11:46 -0500

    add initial solution, quadratic time

--- multiplePointers.js
+++ multiplePointers.js
@@ -2,3 +2,34 @@
 // integers. The function should find the first pair whre the sum
 // is 0. Return an array that includes both values that sum to zero
 // or undefined if a pair does not exist
+
+// Initital solution, quadratic time complexity
+const sumZero = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]];
+      }
+    }
+  }
+};
+
+// console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
+
+// Refactored solution, linear time complexity
+// const sumZero2 = (arr) => {
+//   let left = 0;
+//   let right = arr.length - 1;
+//   while (left < right) {
+//     let sum = arr[left] + arr[right];
+//     if (sum === 0) {
+//       return arr[left], arr[right];
+//     } else if (sum > 0) {
+//       right--;
+//     } else {
+//       left++;
+//     }
+//   }
+// };
+
+// console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
