21b80c935d7586b03a7e18a1ca4fae8d1188672a
Kyle Kauma <kylekauma@gmail.com>
2021-10-13 20:25:58 -0500

    create empty objects to store frequencies, iterate over strings and store values

--- validAnagram.js
+++ validAnagram.js
@@ -2,3 +2,17 @@
 // is an anagram of the first.  An anagram is a word, phrase, or name
 // formed by rearranging the letters of another, such as cinema, formed
 // from iceman.
+
+// First solution
+const validAnagram = (str1, str2) => {
+  if (str1.length !== str2.length) return false;
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+
+  for (let letter of str1) {
+    frequencyCounter1[letter] = (frequencyCounter1[letter] || 0) + 1;
+  }
+  console.log(frequencyCounter1);
+};
+
+validAnagram("hello", "ohlle"); // should be true
