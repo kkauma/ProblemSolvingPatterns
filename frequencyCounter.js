a732653969d06531ffa69d9e082f301a84f9d483
Kyle Kauma <kylekauma@gmail.com>
2021-10-13 19:24:26 -0500

    add naive solution, quadratic time

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -6,3 +6,19 @@
 // The function should return true if every value in the array
 // has it's corresponding value squared in the second array.
 // The frequency of values must be the same
+
+// Naive solution, O(n^2)
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) {
+      return false;
+    }
+    console.log(arr2);
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
