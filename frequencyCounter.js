f1ac7176f23c9e1410a1a216155c987720bdafb0
Kyle Kauma <kylekauma@gmail.com>
2021-10-13 19:23:03 -0500

    add instructions for frequency counter pattern

---
+++ frequencyCounter.js
@@ -0,0 +1,8 @@
+/**********************************/
+/*** Frequency Counter Pattern ***/
+/********************************/
+
+// Write a function called "same", which accepts 2 arrays.
+// The function should return true if every value in the array
+// has it's corresponding value squared in the second array.
+// The frequency of values must be the same
