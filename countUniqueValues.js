cf405f5548b9f5d105b9e36121354ff25b5dc81c
Kyle Kauma <kylekauma@gmail.com>
2021-10-14 20:34:15 -0500

    write out instructions, implement starter code

---
+++ countUniqueValues.js
@@ -0,0 +1,8 @@
+// Implement a function called countUniqueValues, which accepts a
+// sorted array, and counts the unique values in the array.  There
+// can be negative numbers in the array, but it will always be sorted
+
+const countUniqueValues = (arr) => {};
+
+countUniqueValues([1, 1, 1, 1, 1, 1, 2]); // 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7]); // 5
