36f9ab98316dc831bdae7ac7c646a212bca7e7dd
Kyle Kauma <kylekauma@gmail.com>
2021-10-14 20:46:53 -0500

    implement multiple pointers solution

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -2,7 +2,18 @@
 // sorted array, and counts the unique values in the array.  There
 // can be negative numbers in the array, but it will always be sorted
 
-const countUniqueValues = (arr) => {};
+const countUniqueValues = (arr) => {
+  if (arr.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+};
 
-countUniqueValues([1, 1, 1, 1, 1, 1, 2]); // 2
-countUniqueValues([1, 2, 3, 4, 4, 4, 7]); // 5
+console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2])); // 2
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7])); // 5
+console.log(countUniqueValues([])); // 0
