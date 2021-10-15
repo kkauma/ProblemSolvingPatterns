ce5c82c22df8c2437e0926ebd037b40751c73022
Kyle Kauma <kylekauma@gmail.com>
2021-10-14 20:58:24 -0500

    implement initial solution that results in quadratic runtime

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -2,8 +2,22 @@
 // integers and a number called n. The function should calculate the
 // maximum sum of n consecutive elements in the array
 
-const maxSubarraySum = (arr) => {};
-
-const maxSubarraySum = (arr) => {
-  if (arr.length === 0) return null;
+// Initial solution, O(n^2)
+const maxSubarraySum = (arr, num) => {
+  if (num > arr.length) {
+    return null;
+  }
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
 };
+
+console.log(maxSubarraySum([2, 3, 4, 7, 6, 5, 9, 7, 1, 2, 3, 5], 3));
