6515c9734e791b27ca33a755bf1e621e6ac3b9a9
Kyle Kauma <kylekauma@gmail.com>
2021-10-14 20:52:38 -0500

    add instructions, boilerplate code for sliding window pattern

---
+++ maxSubarraySum.js
@@ -0,0 +1,9 @@
+// Write a function called maxSubarraySum which accepts an array of
+// integers and a number called n. The function should calculate the
+// maximum sum of n consecutive elements in the array
+
+const maxSubarraySum = (arr) => {};
+
+const maxSubarraySum = (arr) => {
+  if (arr.length === 0) return null;
+};
