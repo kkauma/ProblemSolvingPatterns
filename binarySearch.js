d19eae0897caa95b558bb0131b69bcdebe943fde
Kyle Kauma <kylekauma@gmail.com>
2021-10-18 22:43:44 -0500

    implement linear search algorigth, linear time complexity

---
+++ binarySearch.js
@@ -0,0 +1,11 @@
+// Linear Search, O(n)
+const linearSearch = (arr, val) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 7));
