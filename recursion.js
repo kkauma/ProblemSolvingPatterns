325827f1b6975ea8218c52df1e2b57772e8ab91a
Kyle Kauma <kylekauma@gmail.com>
2021-10-19 19:50:39 -0500

    add recursive solution to count down function

---
+++ recursion.js
@@ -0,0 +1,10 @@
+// countDown function that uses recursion
+const countDown = (num) => {
+  if (num <= 0) {
+    console.log("All done!");
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+};
